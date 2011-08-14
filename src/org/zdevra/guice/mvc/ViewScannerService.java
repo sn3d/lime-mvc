a3df558ab77ecf7e6462349e55acc2c6315630cc
Zdenko Vrabel <vrabel.zdenko@gmail.com>
2011-08-14 15:02:09 +0100

    Small corrections

--- src/org/zdevra/guice/mvc/ViewScannerService.java
+++ src/org/zdevra/guice/mvc/ViewScannerService.java
@@ -1,3 +1,19 @@
+/*****************************************************************************
+ * Copyright 2011 Zdenko Vrabel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
 package org.zdevra.guice.mvc;
 
 import java.lang.annotation.Annotation;
@@ -23,8 +39,8 @@ import com.google.inject.Singleton;
  * class CustomModule extends MvcModule {
  *    protected void configureControllers() {
  *       ...
- *       registerViewScanner().as(Custom1ViewScanner.class);
- *       registerViewScanner().asInstance(new Custom2ViewScanner());
+ *       registerViewScanner(Custom1ViewScanner.class);
+ *       registerViewScanner(new Custom2ViewScanner());
  *       ...
  *    }
  * }
