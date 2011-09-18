af631ba6db5dcc43d7c789c6cab4f31b5db125be
Zdenko Vrabel <vrabel.zdenko@gmail.com>
2011-09-18 21:40:02 +0200

    The methods in the AbstractConverter has been changed to protected

--- src/org/zdevra/guice/mvc/converters/AbstractConverter.java
+++ src/org/zdevra/guice/mvc/converters/AbstractConverter.java
@@ -1,3 +1,19 @@
+/*****************************************************************************
+ * Copyright 2011 Zdenko Vrabel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
 package org.zdevra.guice.mvc.converters;
 
 import org.zdevra.guice.mvc.ConversionService;
@@ -24,7 +40,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      * @param data
      * @return
      */
-    private final static String getValue(String name, Map<String, String[]> data)
+    protected final static String getValue(String name, Map<String, String[]> data)
     {
         return getValue(name, 0, data);
     }
@@ -37,7 +53,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      * @param data
      * @return
      */
-    private final static int getValueInt(String name, Map<String, String[]> data)
+    protected final static int getValueInt(String name, Map<String, String[]> data)
     {
         return getValueInt(name, 0, data);
     }
@@ -53,7 +69,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      *
      * @return
      */
-    private final static String getValue(String name, int index, Map<String, String[]> data)
+    protected final static String getValue(String name, int index, Map<String, String[]> data)
     {
         String[] values = data.get(name);
         if ((values == null) || (values.length == 0)) {
@@ -72,7 +88,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      *
      * @return
      */
-    private final static int getValueInt(String name, int index, Map<String, String[]> data)
+    protected final static int getValueInt(String name, int index, Map<String, String[]> data)
     {
         String value = getValue(name, index, data);
         return Integer.parseInt(value);
@@ -88,7 +104,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      *
      * @return
      */
-    private final static long getValueLong(String name, int index, Map<String, String[]> data)
+    protected final static long getValueLong(String name, int index, Map<String, String[]> data)
     {
         String value = getValue(name, index, data);
         return Long.parseLong(value);
@@ -104,7 +120,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      *
      * @return
      */
-    private final static double getValueDouble(String name, int index, Map<String, String[]> data)
+    protected final static double getValueDouble(String name, int index, Map<String, String[]> data)
     {
         String value = getValue(name, index, data);
         return Double.parseDouble(value);
@@ -120,7 +136,7 @@ public abstract class AbstractConverter<T> implements ConversionService.Converte
      * @param df
      * @return
      */
-    private final static Date getValueDate(String name, int index, DateFormat df, Map<String, String[]> data)
+    protected final static Date getValueDate(String name, int index, DateFormat df, Map<String, String[]> data)
     {
         try {
             String value = getValue(name, index, data);
