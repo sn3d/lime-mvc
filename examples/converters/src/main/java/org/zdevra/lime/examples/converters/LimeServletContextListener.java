34c473fdf09fd3b40681e18288d36d6194ba818e
Zdenko Vrabel <vrabel.zdenko@gmail.com>
2012-01-26 21:22:56 +0100

    version in examples has been changed

--- examples/converters/src/main/java/org/zdevra/lime/examples/converters/LimeServletContextListener.java
+++ examples/converters/src/main/java/org/zdevra/lime/examples/converters/LimeServletContextListener.java
@@ -5,7 +5,7 @@ import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 import org.zdevra.guice.mvc.MvcModule;
 import org.zdevra.guice.mvc.freemarker.FreemarkerModule;
-import org.zdevra.guice.mvc.freemarker.FreemarkerView;
+import org.zdevra.guice.mvc.freemarker.FreemarkerViewPoint;
 
 /**
  * Context listener prepare a Guice Injector and application's MVC module
@@ -26,7 +26,7 @@ public class LimeServletContextListener extends GuiceServletContextListener {
 
                 //setup views
                 install(new FreemarkerModule(getServletContext()));
-                bindViewName("main").toViewInstance(new FreemarkerView("main.ftl"));
+                bindViewName("main").toViewInstance(new FreemarkerViewPoint("main.ftl"));
             }
         });
 	}
