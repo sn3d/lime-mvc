260bdfb62d41c9c08ba68a9a1d11abba9003c2e6
Tadas Šubonis <tadas.subonis@gmail.com>
2013-01-06 14:05:15 +0000

    Revert "Code formatting velocity extension"
    This reverts commit a98846d83844165d5025ff9da881176aaff7b9ac.

--- extensions/velocity/src/main/java/org/zdevra/guice/mvc/velocity/package-info.java
+++ extensions/velocity/src/main/java/org/zdevra/guice/mvc/velocity/package-info.java
@@ -1,52 +1,68 @@
-/**
- * ***************************************************************************
+/*****************************************************************************
  * Copyright 2011 Zdenko Vrabel
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- *
- ****************************************************************************
- */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ *****************************************************************************/
+
 /**
- * The package implements into Lime MVC a Velocity as a template engine
- * responsible for views rendering. <p>
- *
- * <h3>Usage</h3> Let's assume that we've got controller which selects view with
- * name 'someview'. For rendering of the HTML, we will use a Velocity template
- * 'somehtml.velocity'. <p>
- *
- * <br><b>controller:</b></p> <pre class="prettyprint"> {@literal @}Controller
- * {@literal @}View("someview") public class MyController { ...
- * {@literal @}Path("/helloworld") {@literal @}Model("msg") public String
- * showHelloWorld() { return "Hello World"; } }
- * </pre>
- *
- * <br>the module and view setup will be:<p> <pre class="prettyprint"> public
- * class MyModule extends MvcModule { protected void
- * configureControllers(VelocityEngine velocity) {
- * control("/*").withController(MyController.class);
- *
- *       // setup views install(new VelocityModule(getServletContext()));
- * bindViewName("someview").toViewInstance(new
- * VelocityViewPoint("somehtml.velocity")); } }
- * </pre>
- *
- * There is also another way how to use Velocity view. Directly in the
- * controller. <p>
- *
- * <br><b>example:</b><p> <pre class="prettyprint"> {@literal @}Controller
- * {@literal @}VelocityView("somehtml.velocity") public class MyController { ...
+ * The package implements into Lime MVC a Velocity as a 
+ * template engine responsible for views rendering.   
+ * <p>
+ * 
+ * <h3>Usage</h3>
+ * Let's assume that we've got controller which selects view with name 'someview'.
+ * For rendering of the HTML, we will use a Velocity template 'somehtml.velocity'. 
+ * <p>
+ * 
+ * <br><b>controller:</b></p>
+ * <pre class="prettyprint">
+ * {@literal @}Controller
+ * {@literal @}View("someview")
+ * public class MyController {
+ *    ...
+ *    {@literal @}Path("/helloworld") {@literal @}Model("msg")
+ *    public String showHelloWorld() {
+ *       return "Hello World";
+ *    }
  * }
  * </pre>
- *
+ * 
+ * <br>the module and view setup will be:<p>
+ * <pre class="prettyprint">
+ * public class MyModule extends MvcModule {
+ *    protected void configureControllers(VelocityEngine velocity)
+ *    {
+ *       control("/*").withController(MyController.class);
+ *       
+ *       // setup views       
+ *       install(new VelocityModule(getServletContext()));
+ *       bindViewName("someview").toViewInstance(new VelocityViewPoint("somehtml.velocity"));
+ *    }
+ * }
+ * </pre>
+ * 
+ * There is also another way how to use Velocity view. Directly in the controller.
+ * <p>
+ * 
+ * <br><b>example:</b><p>
+ * <pre class="prettyprint">
+ * {@literal @}Controller
+ * {@literal @}VelocityView("somehtml.velocity")
+ * public class MyController {
+ *    ...
+ * }
+ * </pre>
+ * 
  */
 package org.zdevra.guice.mvc.velocity;
