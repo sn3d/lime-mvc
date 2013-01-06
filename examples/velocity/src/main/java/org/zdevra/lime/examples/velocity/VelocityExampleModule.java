1ccab259d5a7b534fd1a5756a4092b5ecbd13a02
Tadas Šubonis <tadas.subonis@gmail.com>
2013-01-06 13:18:38 +0000

    Code formatting velocity example

--- examples/velocity/src/main/java/org/zdevra/lime/examples/velocity/VelocityExampleModule.java
+++ examples/velocity/src/main/java/org/zdevra/lime/examples/velocity/VelocityExampleModule.java
@@ -4,9 +4,10 @@ import org.zdevra.guice.mvc.MvcModule;
 import org.zdevra.guice.mvc.velocity.VelocityModule;
 
 public class VelocityExampleModule extends MvcModule {
-	@Override
-	protected void configureControllers() {
-		install(new VelocityModule(getServletContext()));
-		control("/*").withController(VelocityExampleController.class);
-	}
+
+    @Override
+    protected void configureControllers() {
+        install(new VelocityModule(getServletContext()));
+        control("/*").withController(VelocityExampleController.class);
+    }
 }
--- examples/velocity/src/main/java/org/zdevra/lime/examples/velocity/VelocityServletContextListener.java
+++ examples/velocity/src/main/java/org/zdevra/lime/examples/velocity/VelocityServletContextListener.java
@@ -4,10 +4,10 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
-
 public class VelocityServletContextListener extends GuiceServletContextListener {
-	@Override
-	protected Injector getInjector() {
-		return Guice.createInjector(new VelocityExampleModule());
-	}
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new VelocityExampleModule());
+    }
 }
