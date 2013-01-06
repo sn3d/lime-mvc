29ee80ab1b198f5aa5325fe96e201de392c44396
Tadas Šubonis <tadas.subonis@gmail.com>
2013-01-06 13:05:33 +0000

    Code formatting freemarker extension

--- extensions/freemarker/src/main/java/org/zdevra/guice/mvc/freemarker/FreemarkerModule.java
+++ extensions/freemarker/src/main/java/org/zdevra/guice/mvc/freemarker/FreemarkerModule.java
@@ -1,19 +1,21 @@
-/*****************************************************************************
+/**
+ * ***************************************************************************
  * Copyright 2011 Zdenko Vrabel
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- *****************************************************************************/
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ ****************************************************************************
+ */
 package org.zdevra.guice.mvc.freemarker;
 
 import javax.servlet.ServletContext;
@@ -25,68 +27,63 @@ import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
 
 /**
- * Use this Lime MVC Module if you want to use Freemarker
- * template engine for rendering views.
- * 
- * The module register and bind freemarker's Configuration
- * instance into Guice as a singleton. 
- * 
+ * Use this Lime MVC Module if you want to use Freemarker template engine for
+ * rendering views.
+ *
+ * The module register and bind freemarker's Configuration instance into Guice
+ * as a singleton.
+ *
  * @see MvcModule
  */
 public class FreemarkerModule extends ViewModule {
-	
-// ------------------------------------------------------------------------
-	
-	private final ServletContext context;
-	private Configuration conf;
-	
-// ------------------------------------------------------------------------
-	
-	/**
-	 * Constructor ov Freemarker module which loads templates from classpath
-	 */
-	public FreemarkerModule() {
-		this(null);
-	}
 
-	/**
-	 * Constructor ov Freemarker module which loads template files
-	 * from WAR as JSPs.
-	 */
-	public FreemarkerModule(ServletContext context) {
-		this.context = context;
-	}
-		
 // ------------------------------------------------------------------------
-	
-	/**
-	 * You will implement the MVC configuration and setup into this method
-	 *  
-	 * @param freemakerConfiguration
-	 */
-	protected void configureFreemarker(Configuration freemakerConfiguration) {
-		if (context != null) {
-			conf.setServletContextForTemplateLoading(context, "/");
-		} else {
-			conf.setClassForTemplateLoading(FreemarkerModule.class, "/");
-		}		
-		conf.setObjectWrapper(new DefaultObjectWrapper());
-	}
-	
+    private final ServletContext context;
+    private Configuration conf;
+
 // ------------------------------------------------------------------------
-	
-	@Override
-	protected final void configureViews() {
-		try {
-			//create freemaker's configuration
-			conf = new Configuration();
-			configureFreemarker(conf);
-			bind(Configuration.class).toInstance(conf);					
-			registerViewScanner(FreemarkerScanner.class);						
-		} finally {
-			conf = null;
-		}
-	}
-		
+    /**
+     * Constructor ov Freemarker module which loads templates from classpath
+     */
+    public FreemarkerModule() {
+        this(null);
+    }
+
+    /**
+     * Constructor ov Freemarker module which loads template files from WAR as
+     * JSPs.
+     */
+    public FreemarkerModule(ServletContext context) {
+        this.context = context;
+    }
+
+// ------------------------------------------------------------------------
+    /**
+     * You will implement the MVC configuration and setup into this method
+     *
+     * @param freemakerConfiguration
+     */
+    protected void configureFreemarker(Configuration freemakerConfiguration) {
+        if (context != null) {
+            conf.setServletContextForTemplateLoading(context, "/");
+        } else {
+            conf.setClassForTemplateLoading(FreemarkerModule.class, "/");
+        }
+        conf.setObjectWrapper(new DefaultObjectWrapper());
+    }
+
+// ------------------------------------------------------------------------
+    @Override
+    protected final void configureViews() {
+        try {
+            //create freemaker's configuration
+            conf = new Configuration();
+            configureFreemarker(conf);
+            bind(Configuration.class).toInstance(conf);
+            registerViewScanner(FreemarkerScanner.class);
+        } finally {
+            conf = null;
+        }
+    }
 // ------------------------------------------------------------------------	
 }
--- extensions/freemarker/src/main/java/org/zdevra/guice/mvc/freemarker/FreemarkerViewPoint.java
+++ extensions/freemarker/src/main/java/org/zdevra/guice/mvc/freemarker/FreemarkerViewPoint.java
@@ -1,19 +1,21 @@
-/*****************************************************************************
+/**
+ * ***************************************************************************
  * Copyright 2011 Zdenko Vrabel
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- *****************************************************************************/
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ ****************************************************************************
+ */
 package org.zdevra.guice.mvc.freemarker;
 
 import java.io.BufferedOutputStream;
@@ -41,66 +43,57 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 /**
- * The view provide rendering of outptu HTML via Freemarker template
- * engine
+ * The view provide rendering of outptu HTML via Freemarker template engine
  */
 public class FreemarkerViewPoint implements ViewPoint {
 
 // ------------------------------------------------------------------------
-	
-	@Inject private Configuration freemarkerConf;
-	private final String templateFile;
-	
-// ------------------------------------------------------------------------
-		
-	/**
-	 * Constructor 
-	 */
-	public FreemarkerViewPoint(String templateFile) {
-		this.templateFile = templateFile;
-	}
-	
-	
-	/**
-	 * The constructor 
-	 */
-	public FreemarkerViewPoint(Configuration freemakerConf, String templateFile) 
-	{
-		this.freemarkerConf = freemakerConf;
-		this.templateFile = templateFile;
-	}
+    @Inject
+    private Configuration freemarkerConf;
+    private final String templateFile;
 
-			
 // ------------------------------------------------------------------------
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public void render(ModelMap model, HttpServlet servlet, HttpServletRequest request, HttpServletResponse response) 
-	{
-		try {
-			//prepare data
-			List<String> attrNames = Collections.list(request.getAttributeNames());
-			Map<String, Object> data = new HashMap<String, Object>();
-			for (String attrName : attrNames) {
-				Object attr = request.getAttribute(attrName);
-				data.put(attrName, attr);
-			}
-			
-			Template template = freemarkerConf.getTemplate(templateFile);
-			
-			//render the output
-			ByteArrayOutputStream bout = new ByteArrayOutputStream(2048);		
-			Writer out = new OutputStreamWriter(new BufferedOutputStream(bout));
-			template.process(data, out);
-			out.flush();
-			response.getWriter().write(bout.toString());
-		} catch (TemplateException e) {
-			throw new FreemarkerViewException(templateFile, request, e);
-		} catch (IOException e) {
-			throw new FreemarkerViewException(templateFile, request, e);
-		}
-	}
-	
-// ------------------------------------------------------------------------
+    /**
+     * Constructor
+     */
+    public FreemarkerViewPoint(String templateFile) {
+        this.templateFile = templateFile;
+    }
 
+    /**
+     * The constructor
+     */
+    public FreemarkerViewPoint(Configuration freemakerConf, String templateFile) {
+        this.freemarkerConf = freemakerConf;
+        this.templateFile = templateFile;
+    }
+
+// ------------------------------------------------------------------------
+    @SuppressWarnings("unchecked")
+    @Override
+    public void render(ModelMap model, HttpServlet servlet, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //prepare data
+            List<String> attrNames = Collections.list(request.getAttributeNames());
+            Map<String, Object> data = new HashMap<String, Object>();
+            for (String attrName : attrNames) {
+                Object attr = request.getAttribute(attrName);
+                data.put(attrName, attr);
+            }
+
+            Template template = freemarkerConf.getTemplate(templateFile);
+
+            //render the output
+            ByteArrayOutputStream bout = new ByteArrayOutputStream(2048);
+            Writer out = new OutputStreamWriter(new BufferedOutputStream(bout));
+            template.process(data, out);
+            out.flush();
+            response.getWriter().write(bout.toString());
+        } catch (TemplateException e) {
+            throw new FreemarkerViewException(templateFile, request, e);
+        } catch (IOException e) {
+            throw new FreemarkerViewException(templateFile, request, e);
+        }
+    }
+// ------------------------------------------------------------------------
 }
