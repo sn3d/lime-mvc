5c2c8c543a579614e4744498a3392c8a2e9d1967
Tadas Šubonis <tadas.subonis@gmail.com>
2013-01-06 13:05:22 +0000

    Code formatting interceptors example

--- examples/interceptors/src/main/java/org/zdevra/lime/examples/interceptors/InterceptorsExampleContextListener.java
+++ examples/interceptors/src/main/java/org/zdevra/lime/examples/interceptors/InterceptorsExampleContextListener.java
@@ -7,32 +7,28 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
-public class InterceptorsExampleContextListener extends GuiceServletContextListener 
-{
-	@Override
-	protected Injector getInjector() 
-	{
-		return Guice.createInjector(
-			new MvcModule() {									
-				@Override
-				protected void configureControllers() 
-				{
-					install(new FreemarkerModule(getServletContext()));
-					
-					//register global log interceptor for all controllers
-					registerGlobalInterceptor(LogInterceptor.class);
-						
-					control("/public/*")
-						.withController(PublicController.class);
+public class InterceptorsExampleContextListener extends GuiceServletContextListener {
 
-					//register security interceptor for concrete controller						
-					control("/secured/*")						
-						.withController(SecuredController.class)
-						.interceptor(SecurityInterceptor.class);
-																		
-				}
-			}
-		);
-	}
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(
+                new MvcModule() {
+            @Override
+            protected void configureControllers() {
+                install(new FreemarkerModule(getServletContext()));
 
+                //register global log interceptor for all controllers
+                registerGlobalInterceptor(LogInterceptor.class);
+
+                control("/public/*")
+                        .withController(PublicController.class);
+
+                //register security interceptor for concrete controller						
+                control("/secured/*")
+                        .withController(SecuredController.class)
+                        .interceptor(SecurityInterceptor.class);
+
+            }
+        });
+    }
 }
